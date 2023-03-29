b5a6035d2a0e790a504799da7dcbd071de8c11ee
henzoparahua <hnz.2894henzop19@gmail.com>
2023-03-29 20:16:46 -0300

    another commit

--- exercicios-php/sintaxe_basica.php
+++ exercicios-php/sintaxe_basica.php
@@ -1,13 +1,23 @@
 <?php
-
-function mensagem(){
-     echo"<br/> Minha primeira função.";
-}
-$num1 = 10;
-$num2 = 5;
+$dolar = 5;
+$bo = 3;
 function somaValor($num1,$num2){
-    return ($num1+$num2);
+    echo "<br/>".($num1+$num2);
 }
-mensagem();
-somaValor();
+
+function dobro($n){
+    $d = $n * 2;
+    echo "<br/> O dobro de ".$n. " = ".$d;
+}
+function dobro1($n){
+    // $d = $n*2;
+    // return $d;
+    return ($n*2);
+}
+$x = 8;
+echo "<br/> O dobro de ".$x." = ".dobro1($x);
+
+
+dobro(6);
+somaValor($dolar,$bo);
 ?>
