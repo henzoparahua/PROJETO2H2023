a2af12bfae8c15c69fa4d6a74ab2ea3862225666
henzoparahua <hnz.2894henzop19@gmail.com>
2023-04-11 22:02:36 -0300

    another commit

---
+++ exercicios-java/menuBanco/src/Model/Menu_DAO.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class Menu_DAO {
+    public static void dados(){
+        
+    }
+    public static void info(){
+        
+    }
+    public static void resp(){
+        
+    }
+}
