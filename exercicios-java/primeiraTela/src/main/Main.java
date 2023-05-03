b28f04fa357c55f3ed15e15d6b3170d6f0b5b9db
henzoparahua <hnz.2894henzop19@gmail.com>
2023-05-02 22:17:35 -0300

    commit

---
+++ exercicios-java/primeiraTela/src/main/Main.java
@@ -0,0 +1,16 @@
+
+package main;
+
+import View.escola;
+
+
+public class Main {
+
+ 
+    public static void main(String[] args) {
+        
+            new escola().setVisible(true);
+ 
+    }
+    
+}
