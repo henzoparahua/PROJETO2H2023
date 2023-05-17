c9f3c0a2828dbf5aafa747186ac81bbe3c8b2613
henzoparahua <hnz.2894henzop19@gmail.com>
2023-05-17 19:14:20 -0300

    java exercises from yesterday

---
+++ exercicios-java/Jokenpo/src/main/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import View.Jokenpo_GUI;
+
+/**
+ *
+ * @author LAB03M05
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    new Jokenpo_GUI().setVisible(true);
+    }
+    
+}
