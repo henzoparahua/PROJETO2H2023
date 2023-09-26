a8bae0593f75c97f58a28356fbe67959bf81e1a6
henzoparahua <hnz.2894henzop19@gmail.com>
2023-09-25 22:09:56 -0300

    stuff

--- exercicios-php/0920/list.php
+++ exercicios-php/0920/list.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="stylesheet.css">
-    <title>Document</title>
+    <title>Lista</title>
 </head>
 <body>
-    <div class="forms-bg">
-    <form action="v1/Api.php?apicall=createhero" method="post" >
+
+<form action="v1/Api.php?apicall=updatehero" method="post" >
 
     <div class="piramide-rinode">
+
+    <input type="text" name="id" placeholder="id" required>
+    <br> 
 
     <input type="text" name="name" placeholder="name" required>
     <br>    
@@ -51,7 +52,6 @@
     </form>
 
 </div>
-
-
+    
 </body>
 </html>
